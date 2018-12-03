65662fbffb6444320c3333582f4792315a532d0f
Alphonso84 <Alphonso.SensleyJR@gmail.com>
2018-12-03 04:00:58 -0800

    began organizing project

---
+++ Oakland Crime Tracker/Networking/Networking.swift
@@ -0,0 +1,10 @@
+//
+//  Networking.swift
+//  Oakland Crime Tracker
+//
+//  Created by user on 12/3/18.
+//  Copyright © 2018 Alphonso Sensley II. All rights reserved.
+//
+
+import Foundation
+import UIKit
